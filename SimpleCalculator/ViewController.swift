cad2cb71dbfc2d1f56e2351122c550d72a7cc12a
sbrick26 <swayambarik@gmail.com>
2021-09-22 17:56:25 -0700

    optional unwrapping

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func subtract(_ sender: Any) {
+        
+        let result : String
+        if let firstTextInput = firstText.text, let secondTextInput = secondText.text {
+            if let firstNum = Int(firstTextInput), let secondNum = Int(secondTextInput){
+                result = String(firstNum - secondNum)
+            } else{
+                result = "Use Integers Only"
+            }
+        } else {
+            result = "Please Input Numbers"
+        }
+        
+        resultLabel.text = result
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        
+        
+        let result : String
+        if let firstTextInput = firstText.text, let secondTextInput = secondText.text {
+            if let firstNum = Int(firstTextInput), let secondNum = Int(secondTextInput){
+                result = String(firstNum + secondNum)
+            } else{
+                result = "Use Integers Only"
+            }
+        } else {
+            result = "Please Input Numbers"
+        }
+        
+        resultLabel.text = result
+       
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        let result : String
+        if let firstTextInput = firstText.text, let secondTextInput = secondText.text {
+            if let firstNum = Int(firstTextInput), let secondNum = Int(secondTextInput){
+                result = String(firstNum / secondNum)
+            } else{
+                result = "Use Integers Only"
+            }
+        } else {
+            result = "Please Input Numbers"
+        }
+        
+        resultLabel.text = result
+    }
+    @IBAction func multiply(_ sender: Any) {
+        let result : String
+        if let firstTextInput = firstText.text, let secondTextInput = secondText.text {
+            if let firstNum = Int(firstTextInput), let secondNum = Int(secondTextInput){
+                result = String(firstNum * secondNum)
+            } else{
+                result = "Use Integers Only"
+            }
+        } else {
+            result = "Please Input Numbers"
+        }
+        
+        resultLabel.text = result
+    }
+    
+    
 }
 
